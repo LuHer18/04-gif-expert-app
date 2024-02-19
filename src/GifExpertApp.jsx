3c059a886067c4a768f6c4dec47b56464e32323b
Luis Herrera <luis1818.lh@gmail.com>
2024-02-19 14:25:29 -0500

    Se agrega el componente Add Category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,19 +1,30 @@
 import { useState } from "react"
+import { AddCategory } from "./components/AddCategory"
 function GifExpertApp () {
 
     const [categories, setCategories ] = useState([ 'One Punch', 'Drangoball'])
+
+    const onAddCategory = (value) => {
+        if (categories.includes(value) ) return;
+        setCategories([value,...categories ])
+    }
+
     return(
         <>
             {/* title */}
             <h1>Gif Expert App</h1>
 
             {/* input */}
+            <AddCategory 
+                //setCategories={setCategories} categories= {categories}
+                onAddCategory={onAddCategory}
+            />    
 
             {/* listado de gift */}
-
+            
             <ol>
                 {categories.map((category) => (
-                    <li key={category}>
+                    <li key= {category}>
                         {category}
                     </li>
                 ))}
